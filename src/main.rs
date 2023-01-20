727820cffdde1a72a46929e64b0e3bc5121af2a5
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2023-01-20 14:29:31 +0100

    add lint

--- src/main.rs
+++ src/main.rs
@@ -161,24 +161,21 @@ fn main() {
     let fixed_charges_r = format!("{:.2}", total_expenses - flex_charges);
 
     println!(
-        "{}",
-        format!("in: {total_income} ({fixed_income_r} fixed, {flex_income} flexible)")
+        "in: {total_income} ({fixed_income_r} fixed, {flex_income} flexible)",
     );
     println!(
-        "{}",
-        format!("out: {total_expenses} ({fixed_charges_r} fixed {flex_charges} flexible)")
+        "out: {total_expenses} ({fixed_charges_r} fixed {flex_charges} flexible)"
     );
-    println!("{}", format!("{} moved", total_transfers));
-    println!("{}", format!("{free_r} free"));
-    println!("{}", format!("{} total savings\n", savings.total));
+    println!("{} moved", total_transfers);
+    println!("{free_r} free");
+    println!("{} total savings\n", savings.total);
 
     savings.savings.iter().for_each(|saving| {
         let ten_year_interest = saving.amount + saving.compound_interest;
         let ten_r = format!("{:.2}", ten_year_interest);
 
         println!(
-            "{}",
-            format!("{}: {} (10 yr: {ten_r})", saving.name, saving.amount)
+            "{}: {} (10 yr: {ten_r})", saving.name, saving.amount
         );
     });
 }
