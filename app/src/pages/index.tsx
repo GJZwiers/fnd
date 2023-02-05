949c8174be63d8ae7e7d2711bc67c1244825d9de
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2023-02-05 11:43:56 +0100

    simplify ui

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import Image from "next/image";
-import reactLogo from "../assets/react.svg";
-import tauriLogo from "../assets/tauri.svg";
-import nextLogo from "../assets/next.svg";
+import scale from "../assets/scale.svg";
+import quill from "../assets/quill.svg";
+import gold from "../assets/gold.svg";
 
-import { Input, Table, useAsyncList, useCollator } from "@nextui-org/react";
+import { Table, useAsyncList, useCollator } from "@nextui-org/react";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -16,51 +16,6 @@ function App() {
     setGreetMsg(await invoke("greet", { payment }));
   }
 
-  const [expenseMsg, setExpenseMsg] = useState("");
-  const [expense, setExpense] = useState("");
-
-  async function addExpense() {
-    setExpenseMsg(await invoke("expense", { expense }));
-  }
-
-  const columns = [
-    {
-      key: "transaction_type",
-      label: "TRANSACTION_TYPE",
-      image: 
-        <Image
-          width={144}
-          height={144}
-          src={nextLogo}
-          className="logo next"
-          alt="Next logo"
-        />
-    },
-    {
-      key: "description",
-      label: "DESCRIPTION",
-      image: 
-      <Image
-        width={144}
-        height={144}
-        src={tauriLogo}
-        className="logo next"
-        alt="Next logo"
-      />
-    },
-    {
-      key: "amount",
-      label: "AMOUNT",
-      image: 
-      <Image
-        width={144}
-        height={144}
-        src={reactLogo}
-        className="logo next"
-        alt="Next logo"
-      />
-    },
-  ];
   const rows = [
     {
       key: "1",
@@ -87,18 +42,6 @@ function App() {
       amount: "200",
     },
   ];
-
-  const [row, setRow] = useState(rows);
-
-  const onChangeInput = (e, key) => {   
-    const { name, value } = e.target
-
-    const editData = row.map((item) =>
-        item.key === key && name ? { ...item, [name]: value } : item
-    )
-
-    setRow(editData)
-  }
 
   const collator = useCollator({ numeric: true });
 
@@ -128,8 +71,8 @@ function App() {
       <h2>Treasure Cove</h2>
 
     <Table
-      aria-label="Example static collection table"
-      css={{ minWidth: "100%", height: "auto" }} //calc($space$14 * 10)
+      aria-label="Le Table"
+      css={{ minWidth: "100%", height: "auto" }} // calc($space$14 * 10)
       sortDescriptor={list.sortDescriptor}
       onSortChange={list.sort}
       compact
@@ -139,7 +82,7 @@ function App() {
           <Image
             width={144}
             height={144}
-            src={nextLogo}
+            src={gold}
             className="logo next"
             alt="Next logo"
           />
@@ -149,7 +92,7 @@ function App() {
           <Image
             width={144}
             height={144}
-            src={tauriLogo}
+            src={quill}
             className="logo next"
             alt="Next logo"
           />
@@ -159,7 +102,7 @@ function App() {
           <Image
             width={144}
             height={144}
-            src={reactLogo}
+            src={scale}
             className="logo next"
             alt="Next logo"
           />
@@ -172,13 +115,7 @@ function App() {
           <Table.Row key={item.description}>
             {(columnKey) => 
               <Table.Cell>
-                <Input
-                  aria-label="test"
-                  name="type"
-                  value={item[columnKey]}
-                  type="text"
-                  onChange={(e) => onChangeInput(e, item[columnKey])}
-                  />
+                {item[columnKey]}
               </Table.Cell>}
           </Table.Row>
         )}
@@ -188,9 +125,9 @@ function App() {
       <div className="row">
         <div>
           <input
-            id="payment-input"
+            id="row-input"
             onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Add earning"
+            placeholder="new"
           />
           <button type="button" onClick={() => addEarning()}>
             Add
@@ -199,21 +136,6 @@ function App() {
       </div>
 
       <p>{greetMsg}</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setExpense(e.currentTarget.value)}
-            placeholder="Add expense"
-          />
-          <button type="button" onClick={() => addExpense()}>
-            Add
-          </button>
-        </div>
-      </div>
-
-      <p>{expenseMsg}</p>
     </div>
   );
 }
